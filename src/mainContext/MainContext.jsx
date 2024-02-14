0b2f02921473f92e28d6b0fc3c50e1d94ad244ff
KGstievK <kagastiev.kudaibergen.kk@gmail.com>
2024-02-14 18:07:55 +0600

    admin function

--- src/mainContext/MainContext.jsx
+++ src/mainContext/MainContext.jsx
@@ -1,10 +1,57 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const mainContext = createContext();
 export const useMainContext = () => useContext(mainContext);
 
+
 const MainContext = ({ children }) => {
-	let values = {};
+
+
+const [admin, setAdmin] = useState([])
+
+const  navigate = useNavigate()
+
+
+const [image, setImage] = useState('')
+const [name, setName] = useState('')
+const [category, setCategory] = useState('')
+const [price, setPrice] = useState('')
+const [discriotion, setDiscriotion] = useState('')
+
+function getSave() {
+	let obj = {
+		image,
+		name,
+		category,
+		price,
+		discriotion,
+		id: Date.now(),
+	}
+	const data = JSON.parse(localStorage.getItem('book')) || []
+	data.push(obj)
+	localStorage.setItem('book', JSON.stringify(data))
+	
+	setImage('')
+	setName('')
+	setCategory('')
+	setPrice('')
+	setDiscriotion('')
+
+	navigate('/')
+}
+
+	let values = {
+		admin, setAdmin,
+		
+	  image,	setImage,
+		name, setName,
+		category, setCategory,
+		price, setPrice,
+		discriotion, setDiscriotion,
+
+		getSave,
+	};
 
 	return <mainContext.Provider value={values}>{children}</mainContext.Provider>;
 };
--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,8 +1,32 @@
+import { useMainContext } from "../../mainContext/MainContext";
+
+
 const Admin = () => {
+	const {getSave, 
+		image, setImage,
+		name,	setName,
+		category, setCategory,
+		price, setPrice,
+		discriotion, setDiscriotion,} = useMainContext()
 	return (
 		<section id="admin">
 			<div className="container">
-				<div className="admin"></div>
+				<div className="admin">
+					<div className="admin__block">
+						<div className="file">
+							<input onChange={(e) => setImage(e.target.value)} type="file" placeholder="Upload photo" value={image}/>
+						</div>
+						<div className="form">
+							<input onChange={(e) => setName(e.target.value)} type="text" placeholder="Product Name" value={name}/>
+							<div className="twoStrok">
+								<input onChange={(e) => setCategory(e.target.value)} type="text" placeholder="Product Category" value={category}/>
+								<input onChange={(e) => setPrice(e.target.value)} type="text" placeholder="Price" value={price}/>
+							</div>
+							<input onChange={(e) => setDiscriotion(e.target.value)} type="text" placeholder="Product description..." value={discriotion}/>
+							<button onClick={() => getSave()}>SAVE</button>
+						</div>
+					</div>
+				</div>
 			</div>
 		</section>
 	);
