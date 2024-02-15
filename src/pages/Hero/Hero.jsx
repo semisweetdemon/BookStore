447fcf57741a235e9a763d73adaac57b28bac377
semisweetdemon <semisweetdemon@gmail.com>
2024-02-15 13:11:51 +0600

    swiper

--- src/pages/Hero/Hero.jsx
+++ src/pages/Hero/Hero.jsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from 'react';
 import BooksBlock from '../../components/BooksBlock/BooksBlock';
 
 const Hero = () => {
-	let swipetImages = [];
+	const [count, setCount] = useState(0);
+	let swiperImages = ['https://images.unsplash.com/photo-1707345512638-997d31a10eaa?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'https://images.unsplash.com/photo-1470770841072-f978cf4d019e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'];
 
 	let data = JSON.parse(localStorage.getItem('book')) || [];
+
+	useEffect(() => {
+		setInterval(() => {
+			setCount((count) => (count >= swiperImages.length - 1 ? 0 : (count += 1)));
+		}, 7000);
+	}, []);
+
+	let arr = [];
+
+	for (let i = 0; i < swiperImages.length; i++) {
+		arr.push(
+			<div
+				onClick={() => {
+					setCount(i);
+				}}
+				className={count === i ? 'swiper__pagination_dot dotnogray' : 'swiper__pagination_dot'}></div>
+		);
+	}
 
 	return (
 		<>
@@ -12,20 +32,25 @@ const Hero = () => {
 					<div className="hero">
 						<div className="hero__swiper swiper">
 							<div className="swiper__arrows">
-								<div className="swiper__arrows_left">
+								<div
+									onClick={() => {
+										setCount((count) => (count <= 0 ? swiperImages.length - 1 : (count -= 1)));
+									}}>
 									<ion-icon name="arrow-back-outline"></ion-icon>
 								</div>
-								<div className="swiper__arrows_right">
+
+								<div
+									onClick={() => {
+										setCount((count) => (count >= swiperImages.length - 1 ? 0 : (count += 1)));
+									}}>
 									<ion-icon name="arrow-forward-outline"></ion-icon>
 								</div>
 							</div>
-							<div className="swiper__image"></div>
+							<div className="swiper__image">
+								<img src={swiperImages[count]} alt="No swiper" />
+							</div>
 							<div className="swiper__pagination">
-								<div className="swiper__pagination_dots">
-									<div className="swiper__pagination_dot dotnogray"></div>
-									<div className="swiper__pagination_dot"></div>
-									<div className="swiper__pagination_dot"></div>
-								</div>
+								<div className="swiper__pagination_dots">{arr.map((el) => el)}</div>
 							</div>
 						</div>
 					</div>
